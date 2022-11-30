a83bba592c5fed8bef20e29be91272957cd5557c
joaobatistafrontend <jbbuno007@gmail.com>
2022-11-30 17:23:54 -0300

    add lisrde... na viiw (ordenacao)

--- core/serializers.py
+++ core/serializers.py
@@ -22,3 +22,9 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from .models import Autor, Categoria, Editora , Livro
-from .serializers import AutorSeerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer
+from .serializers import AutorSeerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetailSerializer
 import json
 from rest_framework import viewsets
 
@@ -20,4 +20,11 @@ class EditoraApiViewSet(viewsets.ModelViewSet):
 
 class LivroApiViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
+    # def get_serializer_class(self):
+    #     if self.action == 'list':
+    #         return LivroDetailSerializer
+    #     if self.action == 'retrieve':
+    #         return LivroDetailSerializer
+    #     return LivroSerializer
+    serializer_class = LivroDetailSerializer
+    # serializer_class = LivroSerializer
